9fef539ca9982fe5d15b94a28560f14ec691391d
CosmoAdryan <adryancosmo56@gmail.com>
2026-04-17 23:18:20 -0300

    Fix build CI: remove import Link nao utilizado no HeaderValdirene

--- src/markup/Layout/HeaderValdirene.js
+++ src/markup/Layout/HeaderValdirene.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import logoValdirene from './../../images/logos/logo_valdirene.png';
 import './HeaderValdirene.css';
 
